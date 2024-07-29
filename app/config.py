f14fc0796674bb38419ff266302b6e63bb4a09f3
starmindz <stanleynnamani3@gmail.com>
2024-07-29 22:22:24 +0100

    Connect to live database

--- app/config.py
+++ app/config.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Get the DATABASE_URL from environment variables
-DATABASE_URL = "postgresql://postgres:Stark*369@localhost:5432/glycobuddy"
-
+# DATABASE_URL = "postgresql://postgres:Stark*369@localhost:5432/glycobuddy"
+LIVE_DATABASE_URL = "postgresql://glycobuddy_database_user:tTPIx1zYECv7ZrUKuz9Ta2gDRDh81VQF@dpg-cqk0c5hu0jms73doftig-a/glycobuddy_database"
 # If using PostgreSQL
-engine = create_engine(DATABASE_URL, echo=True, pool_size=5, max_overflow=20)
-
+# engine = create_engine(DATABASE_URL, echo=True, pool_size=5, max_overflow=20)
+engine = create_engine(LIVE_DATABASE_URL, echo=True, pool_size=5, max_overflow=20)
 # If using SQLite and want to support multiple threads
 # engine = create_engine("sqlite:///./test.db", connect_args={"check_same_thread": False})
 
