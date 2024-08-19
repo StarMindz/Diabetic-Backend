a85058b4e22bec5a29a0e7d2205844cf4ec429e3
starmindz <stanleynnamani3@gmail.com>
2024-08-19 12:33:51 +0100

    Merge conflicts corrected

--- app/api/scan_router.py
+++ app/api/scan_router.py
@@ -41,15 +41,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained=False)
 
 # Load the fine-tuned weights
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-model.load_state_dict(torch.load('clip_finetuned_model\clip_finetuned3.pth', map_location=device)) 
-=======
 model.load_state_dict(torch.load('clip_finetuned_model/clip_finetuned3.pth', map_location=device)) 
->>>>>>> Stashed changes
-=======
-model.load_state_dict(torch.load('clip_finetuned_model/clip_finetuned3.pth', map_location=device)) 
->>>>>>> Stashed changes
+
 
 # Move the model to the correct device and enter eval mode
 model.to(device)
